1328305f87a4a17c910dafecbd375c587d605c74
Dustin <dustykim@gmail.com>
2017-10-11 18:40:39 -0400

    create signup view

--- App.js
+++ App.js
@@ -2,21 +2,36 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/components/Login/Login.js'
 import Main from './src/components/Main.js'
+import Signup from './src/components/Login/Signup.js'
 
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: false,
+      signingUp: false
     }
     this.LogInUser = this.LogInUser.bind(this);
+    this.goToSignUp = this.goToSignUp.bind(this);
+    this.backToLogIn = this.backToLogIn.bind(this);
   }
   
-
   LogInUser() {
     this.setState({
       isLoggedIn: true
+    })
+  }
+
+  goToSignUp() {
+    this.setState({
+      signingUp: true
+    })
+  }
+
+  backToLogIn() {
+    this.setState({
+      signingUp: false
     })
   }
 
@@ -26,9 +41,20 @@ export default class App extends React.Component {
         <Main />
       )
     } else {
-      return (
-        <Login LogInUser={this.LogInUser}/>
-      )
+      if (!this.state.signingUp) {
+        return (
+          <Login 
+            LogInUser={ this.LogInUser } 
+            goToSignUp={ this.goToSignUp }
+            />
+        )
+      } else {
+        return (
+          <Signup 
+            LogInUser={ this.LogInUser } 
+            backToLogIn={ this.backToLogIn }/>
+        )
+      }
     }
   }
 }
